ed21bb76ba2f0e0b48fa2cc400edd3d8218d8b46
Sam-bankole <sbankole495@gmail.com>
2024-01-08 00:09:27 +0100

    like button and database fecthing

---
+++ src/Homepage/likeButton.jsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref as databaseRef,
+  get,
+  set,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { MdFavoriteBorder } from "react-icons/md";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjw6uBT7UdCYFeOEuse-yP7XmAo4wvWkc",
+  authDomain: "real-estate-pro-bdf8b.firebaseapp.com",
+  databaseURL:
+    "https://real-estate-pro-bdf8b-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "real-estate-pro-bdf8b",
+  storageBucket: "real-estate-pro-bdf8b.appspot.com",
+  messagingSenderId: "990812988530",
+  appId: "1:990812988530:web:903cc84c1c10ab05a64eeb",
+  measurementId: "G-3BE0QXKHFV",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const propertiesInDB = databaseRef(db, "propertyList");
+
+const LikeButton = () => {
+  
+
+
+  const [likeCount, setLikeCount] = useState(0);
+  const [liked, setLiked] = useState(false);
+
+  useEffect(() => {
+    // Fetch initial like count when the component mounts
+    likescount();
+  });
+
+function likescount(){
+    onValue(propertiesInDB, function (snapshot) {
+        let propertyId = Object.keys(snapshot.val());
+
+        fetchLikes(propertyId)
+        handleLikeClick(propertyId)
+      });
+}
+
+  const fetchLikes = async (propertyId) => {
+    try {
+      const likesRef = databaseRef(db, `Properties/${propertyId}`);
+      const snapshot = await get(likesRef);
+
+      if (snapshot.exists()) {
+        setLikeCount(snapshot.val().count);
+        setLiked(snapshot.val().liked);
+      }
+    } catch (error) {
+      console.error("Error fetching like count:", error);
+    }
+  };
+
+  const handleLikeClick = async (propertyId) => {
+    try {
+      const likesRef = databaseRef(db, `Properties/${propertyId}`);
+
+      if (!liked) {
+        // If not liked, increment like count
+        await set(likesRef, { count: likeCount + 1, liked: true });
+        setLikeCount((prevCount) => prevCount + 1);
+      } else {
+        // If liked, decrement like count
+        await set(likesRef, { count: likeCount - 1, liked: false });
+        setLikeCount((prevCount) => prevCount - 1);
+      }
+
+      // Toggle liked state
+      setLiked((prevLiked) => !prevLiked);
+    } catch (error) {
+      console.error("Error handling like click:", error);
+    }
+  };
+
+  return (
+    <div>
+      <div className=" flex flex-col items-center">
+        <MdFavoriteBorder onClick={handleLikeClick} />
+        <div className=" text-xs">{likeCount}</div>
+      </div>
+    </div>
+  );
+};
+
+export default LikeButton;
--- src/Homepage/listing.jsx
+++ src/Homepage/listing.jsx
@@ -1,22 +1,58 @@
-import React , { useState } from "react";
+import React , { useEffect, useState } from "react";
 import { LuBed } from "react-icons/lu";
 import { LuBath } from "react-icons/lu";
 import { PiSquareLogo } from "react-icons/pi";
 import { MdFavoriteBorder } from "react-icons/md";
 import { IoEyeOutline } from "react-icons/io5";
 import { PiShareFat } from "react-icons/pi";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, onValue } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+import LikeButton from "./likeButton"
+
+
 
 export default function Listing() {
-//   const ScrollableList = ({ items }) => {
-//     const [scrollPosition, setScrollPosition] = useState(0);
 
-//     const scrollLeft = () => {
-//       setScrollPosition((prevPosition) => prevPosition - 100); // Adjust scroll distance as needed
-//     };
+  const [properties, setProperties] = useState([]);
 
-//     const scrollRight = () => {
-//       setScrollPosition((prevPosition) => prevPosition + 100); // Adjust scroll distance as needed
-//     };
+  useEffect(() => {
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyBjw6uBT7UdCYFeOEuse-yP7XmAo4wvWkc",
+      authDomain: "real-estate-pro-bdf8b.firebaseapp.com",
+      databaseURL:
+        "https://real-estate-pro-bdf8b-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "real-estate-pro-bdf8b",
+      storageBucket: "real-estate-pro-bdf8b.appspot.com",
+      messagingSenderId: "990812988530",
+      appId: "1:990812988530:web:903cc84c1c10ab05a64eeb",
+      measurementId: "G-3BE0QXKHFV",
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const database = getDatabase(app);
+      const addedPropertyInDB = ref(database, 'Properties');
+  
+      const fetchData = () => {
+        onValue(addedPropertyInDB, (snapshot) => {
+          const propertyData = snapshot.val();
+  
+          if (propertyData) {
+            const propertyList = Object.values(propertyData);
+  
+            setProperties(propertyList);
+          }
+        });
+      };
+      fetchData();
+
+      return () => {
+        // Unsubscribe from the database changes
+      };
+  }, [])
+
+
+
 
     return (
       <div className=" mt-8 pl-6 md:pl-3">
@@ -24,43 +60,40 @@ export default function Listing() {
           className=" w-72 bg-slate-100 rounded-lg shadow-lg"
         >
           <div>
-          {/* {items.map((item, index) => ( */}
-            <div>
+         {properties.map((property, index) => (
+            <div key={index}>
               <div className=" relative">
               <img
-                src="https://i.im.ge/2023/12/29/xccZdh.pexels-expect-best-323780-1.jpg"
+                src={property.img}
                 alt="House for sale"
-                className=" rounded-t-xl"
+                className=" rounded-t-xl h-44 w-72"
               />
               <h5 className=" absolute bottom-0 bg-amber-50/75 px-2 py-1 rounded-e-lg text-sm font-bold">
-                N1,000,000/yr
+              ₦{property.price}/{property.subscription}
               </h5>
               <h5 className="absolute bottom-0 right-0 bg-amber-400/75 px-2 py-1 rounded-s-lg text-sm font-bold">
-                Rent
+                {property.terms}
               </h5>
             </div>
             <div className=" p-3">
-              <div className=" font-bold">Title</div>
-              <div className=" text-sm text-gray-500">location</div>
+              <div className=" font-bold">{property.title}</div>
+              <div className=" text-sm text-gray-500">{property.location}</div>
               <div className=" mt-2 flex justify-between items-center pb-2 border-b-2 border-amber-400">
                 <div className=" flex items-center">
                   <LuBed className=" text-base" />{" "}
-                  <div className=" text-xs ml-1">5 bed</div>
+                  <div className=" text-xs ml-1">{property.bed} bed</div>
                 </div>
                 <div className=" mt-2 flex justify-between items-center">
                   <LuBath className=" text-base" />{" "}
-                  <div className=" text-xs ml-1">5 bath</div>
+                  <div className=" text-xs ml-1">{property.bath} bath</div>
                 </div>
                 <div className=" mt-2 flex justify-between items-center">
                   <PiSquareLogo className=" text-base" />
-                  <div className=" text-xs ml-1">100 sqft</div>
+                  <div className=" text-xs ml-1">{property.sqft} sqft</div>
                 </div>
               </div>
               <div className=" pb-2 flex justify-between items-center mt-3">
-                <div className=" flex flex-col items-center">
-                  <MdFavoriteBorder />
-                  <div className=" text-xs">120</div>
-                </div>
+                <LikeButton/>
                 <div className=" flex flex-col items-center">
                   <IoEyeOutline />
                   <div className=" text-xs">233</div>
@@ -69,14 +102,14 @@ export default function Listing() {
                   <PiShareFat />
                   <div className=" text-xs">202</div>
                 </div>
+                
               </div>
             </div>
             </div>
-          {/* ))} */}
+          ))}
 
           </div>
-          {/* <button onClick={scrollLeft}>&lt;</button>
-          <button onClick={scrollRight}>&gt;</button> */}
+          
         </div>
       </div>
     );
